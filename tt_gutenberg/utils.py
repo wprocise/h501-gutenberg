5c56ef2bc9c42fb1de50b3ff69e92f8782efda01
Woods Procise <wprocise@iu.edu>
2025-11-06 12:58:10 -0500

    updates

--- tt_gutenberg/utils.py
+++ tt_gutenberg/utils.py
@@ -1,15 +1,17 @@
 # utils.py
 import pandas as pd
+import os
 
-def load_authors(path="data/gutenberg_authors.csv"):
-    """
+"""
     Load the Gutenberg authors dataset as a pandas DataFrame.
-    Path is relative to the package data folder.
-    """
+    Path is relative to the project root.
+"""
+def load_authors(path="data/gutenberg_authors.csv"):
     return pd.read_csv(path)
 
-def clean_aliases(df, alias_col="alias"):
-    """
+"""
     Return a Series with only valid (non-empty) aliases.
-    """
+"""
+def clean_aliases(df, alias_col="alias"):
     return df[df[alias_col].notna()][alias_col]
+
