abc01a7afc627eb56a706d0b15576ec53197e8b6
Woods Procise <wprocise@iu.edu>
2025-11-06 13:20:47 -0500

    Final updates with list of top author aliases by translation count

--- tt_gutenberg/authors.py
+++ tt_gutenberg/authors.py
@@ -1,12 +1,10 @@
 # authors.py
 from .utils import load_authors, clean_aliases
 
-def list_authors(by_languages=False, alias=True, path="data/gutenberg_authors.csv"):
-    
+def list_authors(by_languages=False, alias=True, path=None):  
     df = load_authors(path)
     authors = clean_aliases(df)
 
     if by_languages:
         return authors.value_counts().index.tolist()
-    else:
-        return authors.unique().tolist()
+    return authors.unique().tolist()
--- tt_gutenberg/utils.py
+++ tt_gutenberg/utils.py
@@ -6,12 +6,12 @@ import os
     Load the Gutenberg authors dataset as a pandas DataFrame.
     Path is relative to the project root.
 """
-def load_authors(path="data/gutenberg_authors.csv"):
+def load_authors(path=None):
+    if path is None:
+        path = "data/gutenberg_authors.csv"
     return pd.read_csv(path)
 
-"""
-    Return a Series with only valid (non-empty) aliases.
-"""
 def clean_aliases(df, alias_col="alias"):
+    """Return only valid, non-empty aliases."""
     return df[df[alias_col].notna()][alias_col]
 
